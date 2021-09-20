742cc5a27d4d44d87d024c8475d99179f12ee016
Rafael Gomes <rafaelg1402@protonmail.com>
2021-09-20 13:40:51 +0100

    update api

---
+++ constants/api.js
@@ -0,0 +1,12 @@
+const WooApi = {
+    url: {
+      wc: "https://pt-recruitment.flywheelsites.com/wp-json/wc/v3/",
+      wp: "https://pt-recruitment.flywheelsites.com/wp-json/wp/v2/"
+    },
+    keys: {
+      consumerKey: "ck_f6254f10f53cc892207c8ccdb93e68d6a21e7e34",
+      consumerSecret: "cs_2ad2ff82fc196f2513ae8271daf1688bfc99fb78"
+    }
+  };
+  
+  export default WooApi;
--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,30 @@ import Header from "../src/components/layouts/header";
 import Footer from "../src/components/layouts/footer";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
+import WooApi from "../constants/api";
 import Trending from "../src/components/layouts/trending";
 import CategoriesMain from "../src/components/layouts/categories/categoriesHero";
 import Categories from "../src/components/layouts/categories";
 import Product from "../src/components/layouts/product";
 
-export default function Home({ data }) {
+export default function Home({ products }) {
   return (
     <div className={styles.container}>
-      <Header data={data.products} />
-      <Trending data={data.products} />
-      <CategoriesMain data={data.products} />
-      <Categories data={data.products} />
-      <Product data={data.products} />
+      <Header data={products} />
+      <Trending data={products} />
+      <CategoriesMain data={products} />
+      <Categories data={products} />
+      <Product data={products} />
       <Footer />
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const { data } = await axios.get("http://localhost:3000/api/get-products");
-
+Home.getInitialProps = async () => {
+  const products = await axios.get(
+    `${WooApi.url.wc}products?per_page=100&consumer_key=${WooApi.keys.consumerKey}&consumer_secret=${WooApi.keys.consumerSecret}`
+  );
   return {
-    props: {
-      data: data || {},
-    },
-    revalidate: 60,
+    products: products.data,
   };
-}
+};
